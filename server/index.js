6a9086be5e3a1b6b141b7ff345043ea33332dda6
Bart <bartwijnants@outlook.com>
2016-12-24 13:07:19 +0100

    persisten storage

--- server/index.js
+++ server/index.js
@@ -4,12 +4,19 @@ var db = pgp('postgres://pender:p3nd@db:5432/pend');
 var app = express();
 
 app.get('/', function (req, res) {
-    db.query('select * from information_schema.tables').then(function (data) {
+    db.query('select * from foo').then(function (data) {
         res.send('Data:' + JSON.stringify(data));
     }).catch(function (error) {
         res.send('Error:' + error);
     });
 });
+app.post('/', function (req, res) {
+    db.query('INSERT INTO foo (bar, baz) VALUES (\'such awesome\', 666);').then(function (data) {
+        res.send('Data:' + JSON.stringify(data));
+    }).catch(function (error) {
+        res.send('Error:' + error);
+    });
+})
 
 app.listen(3000, function () {
     console.log('hello world from express in docker!');
