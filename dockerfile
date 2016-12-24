c927614a4097ad043c33b0bd8887af7b036b6d7c
Bart Wijnants <bartwijnants@outlook.com>
2016-12-24 08:34:39 +0100

    express

--- dockerfile
+++ dockerfile
@@ -3,4 +3,6 @@ RUN mkdir /app
 WORKDIR /app
 COPY ./package.json /app/package.json
 COPY ./index.js /app/index.js
+RUN npm install
+EXPOSE 3000
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello world, you are seeing this from docker!')
+})
+
+app.listen(3000, function () {
+  console.log('hello world from express in docker!')
+})
